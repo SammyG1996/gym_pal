0e91990144f096e321196f7ec4a7987b52e4e6a3
Samuel Gonzalez Jr <95721703+SammyG1996@users.noreply.github.com>
2023-09-26 13:50:06 -0400

    updated style on home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -20,7 +20,7 @@ const Home = () => {
   },[])
   return (
     <div className='flex justify-center w-full h-screen'>
-        <div className={`animate-fade-up animate-once animate-duration-[1000ms] animate-ease-out animate-normal animate-fill-forwards bg-[${Colors.dark}] mt-10 bg-opacity-50 backdrop-blur-xl backdrop-filter backdrop-saturate-200 p-5 md:p-10 shadow-xl rounded-lg h-[80vh] md:h-[60vh] max-w-[1000px] w-[92vw] md:w-[50vw] flex flex-col justify-center items-center`}>
+        <div className={`animate-fade-up animate-once animate-duration-[1000ms] animate-ease-out animate-normal animate-fill-forwards bg-[${Colors.dark}] mt-10 bg-opacity-50 backdrop-blur-xl backdrop-filter backdrop-saturate-200 p-5 md:p-10 shadow-xl rounded-lg h-[70vh] md:h-[60vh] max-w-[1000px] w-[92vw] md:w-[50vw] flex flex-col justify-center items-center`}>
             <div>
                 <img className='w-[100vw] mx-auto md:w-[35vw]' src={Logo} alt="logo img" />
                 <h1 className='text-white font-sans text-xl mt-2'>Your ultimate gym companion</h1>
